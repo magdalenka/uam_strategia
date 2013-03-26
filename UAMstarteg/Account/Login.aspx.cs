d207ced0771034f2ebd24f5e0f34e1ef2ffffa77
gmagdaa <gmagdaa@op.pl>
2013-03-26 15:45:59 +0100

    Dodanie biblioteki
    biblioteka

--- UAMstarteg/Account/Login.aspx.cs
+++ UAMstarteg/Account/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Novell.Directory.Ldap;
+using System.Data;
 
 public partial class Account_Login : System.Web.UI.Page
 {
