f62bd4f36f58e336d7de029726d14e1a1be70292
Emilia C. <turboelo.2.0@gmail.com>
2013-05-09 21:35:14 +0200

    klasa do robienia selectów
    żeby nie trzeba było za każdym razem wpisywać kilku linijek + connection
    stringa, bo jak sie zmieni to bedzie problem zeby go zmienic w wielu
    lokalizacjach

---
+++ UAMstarteg/App_Code/DataFetcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Text;
+using System.IO;
+using System.Configuration;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Contains methods to fetch data from database
+/// </summary>
+public class DataFetcher
+{
+	public DataFetcher()
+	{
+	}
+
+    public DataTable getSelectResultsAsDataTable(String query)
+    {
+        SqlConnection mySQLConnection = new SqlConnection();
+        mySQLConnection.ConnectionString = @"Data Source=mssql.wmi.amu.edu.pl;Initial Catalog=uamstrateg;User ID=uamstrateg;Password=21hMpA8a";
+        mySQLConnection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, mySQLConnection);
+        cmd.ExecuteNonQuery();
+
+        SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+        DataTable dataTable = new DataTable();
+        dataAdapter.Fill(dataTable);
+
+        mySQLConnection.Close();
+
+        return dataTable;
+    }
+
+    public SqlDataAdapter getSelectResultsAsSQLDataAdapter(String query)
+    {
+        SqlConnection mySQLConnection = new SqlConnection();
+        mySQLConnection.ConnectionString = @"Data Source=mssql.wmi.amu.edu.pl;Initial Catalog=uamstrateg;User ID=uamstrateg;Password=21hMpA8a";
+        mySQLConnection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, mySQLConnection);
+        cmd.ExecuteNonQuery();
+
+        SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+
+        mySQLConnection.Close();
+
+        return dataAdapter;
+    }
+}
