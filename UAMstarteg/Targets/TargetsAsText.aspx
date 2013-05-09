a4ae7249d38d89c4c49b118a2ab04fbcb864e8ba
Emilia C. <turboelo.2.0@gmail.com>
2013-05-09 21:36:03 +0200

    zmiana wygladu tabeli
    + dodatkowe selecty - narazie do niczego nie potrzebne

--- UAMstarteg/Targets/TargetsAsText.aspx
+++ UAMstarteg/Targets/TargetsAsText.aspx
@@ -11,11 +11,55 @@
             return false;
         }
     </script>
+    <style type="text/css">
+        .lpCells
+        {
+            width: 100px;
+            margin-right: 10px;
+            text-align: center;
+            text-decoration: bold;
+            color: #666633;
+            background-color: #cccc99; 
+            -moz-box-shadow: 3px 4px 5px #999966;
+            -webkit-box-shadow: 3px 4px 5px #999966;       
+        }
+        
+        .contentCells
+        {
+            margin-right: 10px;
+            margin-left: 10px;
+            margin-bottom: 10px;
+            text-align: center;
+            text-decoration: none;
+            color: #333300;
+            background-color: #cccc66; 
+            padding-top:20px;
+            padding-bottom:20px;
+            padding-right:40px;
+            padding-left:40px;
+            -moz-box-shadow: 0px 0px 5px #999933;
+            -webkit-box-shadow: 0px 0px 5px #999933; 
+        }
+        
+        .buttonCells
+        {
+            margin-right: 10px;
+            margin-left: 10px;
+            margin-bottom: 10px;
+            background-color: #cccc33; 
+            padding-top:20px;
+            padding-bottom:20px;
+            padding-right:8px;
+            padding-left:8px;
+            -moz-box-shadow: 0px 0px 5px #999933;
+            -webkit-box-shadow: 0px 0px 5px #999933; 
+        }
+    </style>
 </head>
-<body style="height: 492px">
+<body style="height: 492px" bgcolor = "#535353">
     <form id="form1" runat="server">
     <asp:Table ID="TargetTable" runat="server" BorderStyle="None" 
-        GridLines="Horizontal" Height="215px" HorizontalAlign="Right" Width="719px">
+        GridLines="Horizontal" Height="215px" HorizontalAlign="Left" Width="719px">
     </asp:Table>
     </form>
 </body>
--- UAMstarteg/Targets/TargetsAsText.aspx.cs
+++ UAMstarteg/Targets/TargetsAsText.aspx.cs
@@ -13,80 +13,100 @@ using System.IO;
 using System.Xml;
 using System.Configuration;
 using System.Data.SqlClient;
-// Autor: E.C. wszelkie zażalenia kierować do autora. :)
+
 public partial class TargetsAsText : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    { 
-        int strategyNr = 3; //jakos to przekazać w parametrze
-        SqlConnection mySQLConnection = new SqlConnection();
-        mySQLConnection.ConnectionString = @"Data Source=mssql.wmi.amu.edu.pl;Initial Catalog=uamstrateg;User ID=uamstrateg;Password=21hMpA8a";
-        mySQLConnection.Open();
+    {
+       
+        //Do przekazania z drzewa - co jest rozwinięte (strategia, cel czy dzialanie) i id tego czegos rozwiniętego
+        // jezeli bedzie kilka galezi rozwiniętch jednoczesnie? (? lista parametrow? po przecinku ?)
+        int strategyNr = 3, targetNr = 16, subtargetNr = 27, dzialNr = 6;// do testow
 
-        SqlCommand cmd;
-        //id_parent is null and // tylko cele, narazie podcele tez wyswietla
-        cmd = new SqlCommand("SELECT id, lp, tresc from cel where id_strategii = "+ strategyNr +";", mySQLConnection);
-        cmd.ExecuteNonQuery();
 
-        SqlDataAdapter da = new SqlDataAdapter(cmd);
-        DataTable dt = new DataTable();
-        da.Fill(dt);
+        DataFetcher dataFetcher = new DataFetcher();
+        String query;
+
+        //Wszystkie strategie
+        query = "SELECT id, id_parent, lp, nazwa_strategii AS tresc, nazwa_jednostki, widocznosc FROM strategia";
+        SqlDataAdapter strategies = dataFetcher.getSelectResultsAsSQLDataAdapter(query);
+
+        //wszystkie cele
+        query = "SELECT id, lp, id_strategii, id_parent, tresc, widocznosc FROM cel WHERE id_parent is null";
+        SqlDataAdapter targets = dataFetcher.getSelectResultsAsSQLDataAdapter(query);
+
+        //podcele do konkretnego celu
+        query = "SELECT id, lp, id_strategii, id_parent, tresc, widocznosc FROM cel "
+            + "WHERE id_parent = " + targetNr + ";";
+        SqlDataAdapter subTargets = dataFetcher.getSelectResultsAsSQLDataAdapter(query);
+
+        //dzialania do konkretnego celu/podcelu
+        query = "SELECT * FROM dzialanie_cel INNER JOIN dzialanie " +
+            "ON dzialanie_cel.id_celu = " + targetNr +";";
+        SqlDataAdapter operations = dataFetcher.getSelectResultsAsSQLDataAdapter(query);
+
+        DataTable dt = dataFetcher.getSelectResultsAsDataTable("SELECT id, lp, tresc from cel where id_strategii = " + strategyNr + ";");
+       
         int no = dt.Rows.Count;
+
 
         for (int i=0; i<no; i++) // dla wszytskich wyciagnietych wierszy
         {
             TableRow row = new TableRow();
+           
             String id = dt.Rows[0]["id"].ToString();
-            
+
             //liczba porządkowa
             TableCell lpCell = new TableCell();
-            lpCell.Controls.Add(new LiteralControl(strategyNr + "." + dt.Rows[i][0].ToString()));
+            lpCell.CssClass = "lpCells";
+            lpCell.Controls.Add(new LiteralControl(strategyNr + "." + dt.Rows[i]["lp"].ToString()));
             row.Cells.Add(lpCell);
 
             //tresc
             TableCell contentCell = new TableCell();
-            contentCell.Controls.Add(new LiteralControl(dt.Rows[i][1].ToString()));
+            contentCell.CssClass = "contentCells";
+            contentCell.Controls.Add(new LiteralControl(dt.Rows[i]["tresc"].ToString()));
             row.Cells.Add(contentCell);
 
+            // wszystkie guziki
+            TableCell buttonCell = new TableCell();
+            buttonCell.CssClass = "buttonCells";
+            buttonCell.Width = 66;
+
             //dodaj
-            TableCell addButtonCell = new TableCell();
             ImageButton addButton = new ImageButton();
-            addButton.ID = "addButton_" + id;
-            addButton.ImageUrl = "add.gif";
+           // addButton.ID = "addButton_" + id;
+            addButton.ImageUrl = "add.png";
             addButton.Enabled = true;
             addButton.Visible = true;
             addButton.Height = 22;
             addButton.Width = 22;
             addButton.OnClientClick = "javascript:Popup(" + id + "," + strategyNr + "," + 0 + ");";
-            addButtonCell.Controls.Add(addButton);
-            row.Cells.Add(addButtonCell);
+            buttonCell.Controls.Add(addButton);
 
             //edytuj
-            TableCell editButtonCell = new TableCell();
             ImageButton editButton = new ImageButton();
-            editButton.ID = "editButton_" + id;
-            editButton.ImageUrl = "edit.gif";
+           // editButton.ID = "editButton_" + id;
+            editButton.ImageUrl = "edit.png";
             editButton.Enabled = true;
             editButton.Visible = true;
             editButton.Height = 22;
             editButton.Width = 22;
             editButton.OnClientClick = "javascript:Popup(" + id + "," + strategyNr + "," + 1 + ");";
-            editButtonCell.Controls.Add(editButton);
-            row.Cells.Add(editButtonCell);
+            buttonCell.Controls.Add(editButton);
 
             //usun
             //niebezpiczne usuwanie
-            TableCell deleteButtonCell = new TableCell();
             ImageButton deleteButton = new ImageButton();
-            deleteButton.ID = "deleteButton_" + id;
-            deleteButton.ImageUrl = "delete.gif";
+          //  deleteButton.ID = "deleteButton_" + id;
+            deleteButton.ImageUrl = "delete.png";
             deleteButton.Enabled = true;
             deleteButton.Visible = true;
             deleteButton.Height = 22;
             deleteButton.Width = 22;
             deleteButton.OnClientClick = "javascript:Popup(" + id + "," + strategyNr + "," + 1 + ");";
-            deleteButtonCell.Controls.Add(deleteButton);
-            row.Cells.Add(deleteButtonCell);
+            buttonCell.Controls.Add(deleteButton);
+            row.Cells.Add(buttonCell);
             TargetTable.Rows.Add(row);
         }
     }
